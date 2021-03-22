96fee7a915c8630f03c8bff053b3a95433d04a4e
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2021-03-22 11:06:55 +0000

    Add clearer images for white disk

--- disk-white.py
+++ disk-white.py
@@ -15,6 +15,6 @@ with picamera.PiCamera() as camera:
     camera.awb_mode = 'off'
     camera.awb_gains = g
     # Finally, take several photos with the fixed settings
-    for i in range(17):
+    for i in range(1):
         camera.capture('pictures/' +imgobject + '/' + imgobject + '_' + str(i) + '_' +  str(uuid.uuid4()) + '.jpg')
     # camera.capture_sequence(['pictures/'+imgobject+'/'+imgobject+'_image%02d-' + str(uuid.uuid4()) + '.jpg' % i for i in range(17)])
